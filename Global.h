9c9ed4ed255d675d2de02c4e5a3eded724f42cfa
ReoMiyazaki <k021g1153@g.neec.ac.jp>
2022-07-14 15:44:06 +0900

    線形、二分、ハッシュ探索の仕組み

---
+++ Global.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "DxLib.h"
+#include "stdlib.h"
+#include "time.h"
+
+// ウィンドウのタイトルに表示する文字列
+const char TITLE[] = "ターゲット";
+
+// ウィンドウ横幅
+const int WIN_WIDTH = 640;
+
+// ウィンドウ縦幅
+const int WIN_HEIGHT = 480;
